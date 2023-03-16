d98bc1c2448dd2407ca5b18327e69f94abadd840
Abdullah İbrahim Durusoy <99260593+marasama@users.noreply.github.com>
2023-03-16 12:43:54 +0300

    Add files via upload

--- 3 - get_next_line/get_next_line.h
+++ 3 - get_next_line/get_next_line.h
@@ -1,35 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adurusoy <adurusoy@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/15 17:39:53 by adurusoy          #+#    #+#             */
-/*   Updated: 2023/03/15 17:39:53 by adurusoy         ###   ########.fr       */
+/*   Created: 2023/03/15 17:43:19 by adurusoy          #+#    #+#             */
+/*   Updated: 2023/03/15 17:43:19 by adurusoy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
 # include <unistd.h>
 # include <stdio.h>
 
-char *get_next_line(int fd)
-{
-    static int  a;
-    char    *c;
-    int     b;
+char *get_next_line(int fd);
 
-    a = 1;
-    while (b != 0)
-    {
-        c = malloc(sizeof(char) * a + 1);
-        c[a + 1] = '\0';
-        b = read(fd, c, a);
-        if (c[a] == '\n')
-            return (c);
-        free(c);
-        a++;
-    }
-    return(NULL);
-}
+#endif
