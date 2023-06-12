1baf16d61b2f94d35005f3a688ed57aa3ebbf486
Abdullah İbrahim Durusoy <99260593+marasama@users.noreply.github.com>
2023-06-12 09:12:39 +0300

    Add files via upload

--- 3 - get_next_line/get_next_line.c
+++ 3 - get_next_line/get_next_line.c
@@ -5,31 +5,69 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: adurusoy <adurusoy@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/15 17:39:53 by adurusoy          #+#    #+#             */
-/*   Updated: 2023/03/15 17:39:53 by adurusoy         ###   ########.fr       */
+/*   Created: 2023/06/11 12:05:03 by adurusoy          #+#    #+#             */
+/*   Updated: 2023/06/11 23:40:50 by adurusoy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "get_next_line.h"
-# include <unistd.h>
-# include <stdio.h>
+
+int ft_strlen(char *b)
+{
+	int		a;
+
+	a = 0;
+	while (b[a])
+		a++;
+	return (a);
+}
+
+char *plate(char *piece, char *cake, int count, int *needmore)
+{
+	char	*newpiece;
+	int		a;
+	int		b;
+
+	b = ft_strlen(piece);
+	a = 0;
+	newpiece = (char *)malloc(sizeof(char) * ((*needmore + 1) * BUFFER_SIZE) + 1);
+	if(!newpiece)
+		return (NULL);
+	while(a < b)
+		newpiece[a] = piece[a++];
+	b = 0;
+	while(cake[b] != '\0' || cake[b] != '\n')
+		newpiece[a++] = cake[b++];
+	newpiece[a] = '\0';
+	free(piece);
+}
 
 char *get_next_line(int fd)
 {
-    static int  a;
-    char    *c;
-    int     b;
+	static char	*cake;
+	char		*piece;
+	int			check;
+	int			count;
+	int			needmore;
 
-    a = 1;
-    while (b != 0)
-    {
-        c = malloc(sizeof(char) * a + 1);
-        c[a + 1] = '\0';
-        b = read(fd, c, a);
-        if (c[a] == '\n')
-            return (c);
-        free(c);
-        a++;
-    }
-    return(NULL);
+	cake = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	needmore = 0;
+	piece = NULL;
+	while (1)
+	{
+		count = 0;
+		check = read(fd, cake, BUFFER_SIZE);
+		cake[check] = '\0';
+		if (check == -1 || !cake || check == 0)
+			return (NULL);
+		while (cake[count] != '\n' || cake[count] != '\0')
+			count++;
+		piece = plate(piece, cake, count, &needmore);
+		if (count > BUFFER_SIZE)
+			return (piece);
+	}
 }
--- 3 - get_next_line/get_next_line.h
+++ 3 - get_next_line/get_next_line.h
@@ -5,17 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: adurusoy <adurusoy@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/15 17:43:19 by adurusoy          #+#    #+#             */
-/*   Updated: 2023/03/15 17:43:19 by adurusoy         ###   ########.fr       */
+/*   Created: 2023/06/11 12:05:33 by adurusoy          #+#    #+#             */
+/*   Updated: 2023/06/11 12:06:51 by adurusoy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
 
-# include <unistd.h>
-# include <stdio.h>
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 1000
+#endif
 
-char *get_next_line(int fd);
+
 
 #endif
